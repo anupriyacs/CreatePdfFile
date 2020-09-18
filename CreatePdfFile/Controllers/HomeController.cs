0249387dd5f72b25dcbce7e14bcf02c5945d4c3e
hp <hp@LAPTOP-1QK0A698>
2020-09-18 10:23:23 +0530

    PdffileCreation

--- CreatePdfFile/Controllers/HomeController.cs
+++ CreatePdfFile/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CreatePdfFile.Models;
 using Syncfusion.Pdf;
@@ -7,15 +6,18 @@ using Syncfusion.Pdf.Graphics;
 using System.IO;
 using Syncfusion.Drawing;
 
+
+
+
 namespace CreatePdfFile.Controllers
 {
 	public class HomeController : Controller
     {
-		private const string V = "Photo.jpg";
-
+        private const string V = "Photo.jpg";
+		
 		public object Doc { get; private set; }
 
-		public IActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
@@ -43,76 +45,8 @@ namespace CreatePdfFile.Controllers
         public ActionResult CreateDocument()
         {
 
-            //Create a new PDF document
-            PdfDocument document = new PdfDocument();
 
-            //Add a page to the document
-            PdfPage page = document.Pages.Add();
-
-            //Create PDF graphics for the page
-            PdfGraphics graphics = page.Graphics;
-
-            //Set the standard font
-            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 13);
-
-            //Draw the text
-
-            graphics.DrawString(" Personal Profile ", font, PdfBrushes.Blue, new PointF(15, 0));
-            graphics.DrawString("Fulll Name     : Anupriya cs", font, PdfBrushes.Black, new PointF(0, 25));
-            graphics.DrawString("Date of Birth  :20/01/1995", font, PdfBrushes.Black, new PointF(0, 50));
-            graphics.DrawString("Email          :anu.@1995gmail.com", font, PdfBrushes.Black, new PointF(0, 75));
-
-
-
-
-            graphics.DrawString("Education", font, PdfBrushes.Blue, new PointF(205, 100));
-            graphics.DrawString("Masters Degree  :JMC, 72%,2019  ", font, PdfBrushes.Black, new PointF(205, 120));
-            graphics.DrawString("Bachelors Degre : St. Marys College,Thrissut 73%,2016", font, PdfBrushes.Black, new PointF(205, 140));
-            graphics.DrawString("Higher secondary: St.Aloysius, Thrsissur 73 %, 2013", font, PdfBrushes.Black, new PointF(205, 160));
-            graphics.DrawString("Matriculationl  :I.J.G.H.S, Thrsissur 73 %, 2011", font, PdfBrushes.Black, new PointF(205, 180));
-            graphics.DrawString("Experience ", font, PdfBrushes.Blue, new PointF(205, 220));
-            graphics.DrawString("ABC Solutions 03 / 06 / 2018 - 05 / 09 / 2019", font, PdfBrushes.Black, new PointF(205, 240));
-
-            graphics.DrawString("Achievements", font, PdfBrushes.Blue, new PointF(205, 280));
-            graphics.DrawString("Developed a new employee orientation program that ", font, PdfBrushes.Black, new PointF(205, 300));
-            graphics.DrawString("100% of the company locations adopted", font, PdfBrushes.Black, new PointF(205, 320));
-
-            graphics.DrawString("Projects", font, PdfBrushes.Blue, new PointF(205, 360));
-            graphics.DrawString("Pocket Certificate , 3 / 03 / 2019 - 05 / 06 / 2019" ,font, PdfBrushes.Black, new PointF(205, 380));
-            graphics.DrawString("Python Django web based application to uploading and download certificate with a key", font, PdfBrushes.Black, new PointF(205, 400));
-            graphics.DrawString("download certificate with a key", font, PdfBrushes.Black, new PointF(205, 420));
-
-
-
-            graphics.DrawString("Skills", font, PdfBrushes.Blue, new PointF(40, 120));
-            graphics.DrawString("Technical skills: html, css, c, c++", font, PdfBrushes.Black, new PointF(0, 140));
-
-            graphics.DrawString("Additional skills : Leadership", font, PdfBrushes.Black, new PointF(0, 160));
-
-            //Saving the PDF to the MemoryStream
-            MemoryStream stream = new MemoryStream();
-
-
-/////////////////////////////////////////////////////////////////////////////
-FileStream imageStream = new FileStream("C:\\Users\\hp\\AppData\\Local\\Temp\\CreatePdfFile_CoreWeb-983162689\\CreatePdfFile\\CreatePdfFile\\images\\Photo.jpg", FileMode.Open, FileAccess.Read);
-            PdfBitmap image = new PdfBitmap(imageStream);
-            //Draw the image
-            graphics.DrawImage(image, 0, 0);
-			//Save the PDF document to stream
-			_ = new MemoryStream();
-			doc.Save(stream);
-
-
-
-            document.Save(stream);
-
-            //If the position is not set to '0' then the PDF will be empty.
-            stream.Position = 0;
-
-            //Download the PDF document in the browser.
-            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
-            fileStreamResult.FileDownloadName = "Anuresume.pdf";
-            return fileStreamResult;
+            return View();
 
         }
 
@@ -121,98 +55,187 @@ FileStream imageStream = new FileStream("C:\\Users\\hp\\AppData\\Local\\Temp\\Cr
         public ActionResult CreateDocument1(PersonalDetails details)
         {
 
-			//Create a new PDF document
-			PdfDocument document = new PdfDocument();
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+
+
+            //Create a header and draw the image.
+
+            //RectangleF bounds = new RectangleF(0, 0, pdfDocument.Pages[0].GetClientSize().Width, 50);
+
+
+
+
+
+            //Set margin for all the pages
+
+            document.PageSettings.Margins.All = 30;
+
+
+            
+
 
             //Add a page to the document
             PdfPage page = document.Pages.Add();
+
+
+
+            
 
             //Create PDF graphics for the page
             PdfGraphics graphics = page.Graphics;
 
 
 
-            //Set the standard font
-            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 13);
+			//Set the standard font
+			PdfFont font = new PdfStandardFont(PdfFontFamily.TimesRoman, 11);
+
+
+
+
+
+
+            // Set the color space.
+            graphics.ColorSpace = PdfColorSpace.GrayScale;
+            //Get the graphics client size.
+            SizeF clientSize = graphics.ClientSize;
+            //Draw rectangle to PDF graphics.
+            graphics.DrawRectangle(PdfBrushes.Linen, new RectangleF(PointF.Empty, clientSize));
+
+
+           
+
+
+
+
+
 
 
             //Draw the text
 
 
-            graphics.DrawString(" Personal Profile ", font, PdfBrushes.Blue,new PointF());
-            graphics.DrawString("Fulll Name     : "+details.Name, font, PdfBrushes.Black, new PointF(0,25));
-            graphics.DrawString("Date of Birth  :"+details.Date_birth, font, PdfBrushes.Black, new PointF(0,50));
-            graphics.DrawString("Email             :"+details.Email, font, PdfBrushes.Black, new PointF(0,75));
-    
+            graphics.DrawString("Personal Profile ", font, PdfBrushes.DarkBlue, new PointF(5, 250));
+            graphics.DrawString("---------------------------- ",font, PdfBrushes.Black, new PointF(5, 260));
+            graphics.DrawString("Date Birth      :" + details.Date_birth, font, PdfBrushes.Black, new PointF(5, 280));
+            graphics.DrawString("Email            :" + details.Email, font, PdfBrushes.Black, new PointF(5, 310));
+            graphics.DrawString("Phone           :" + details.Phone, font, PdfBrushes.Black, new PointF(5, 340));
+            graphics.DrawString("Address        :" + details.Address, font, PdfBrushes.Black, new PointF(5, 370));
+            graphics.DrawString("Country        :" + details.Country, font, PdfBrushes.Black, new PointF(5, 410));
+            graphics.DrawString("State            :" + details.State, font, PdfBrushes.Black, new PointF(5, 430));
+            graphics.DrawString("City              :" + details.City, font, PdfBrushes.Black, new PointF(5, 460));
+			
 
-            graphics.DrawString("Education", font, PdfBrushes.Blue, new PointF(205, 100));
-            graphics.DrawString("Masters Degree  :JMC, 72%,2019  ", font, PdfBrushes.Black, new PointF(205, 120));
-            graphics.DrawString("Bachelors Degre : St. Marys College,Thrissut 73%,2016", font, PdfBrushes.Black, new PointF(205, 140));
-            graphics.DrawString("Higher secondary: St.Aloysius, Thrsissur 73 %, 2013", font, PdfBrushes.Black, new PointF(205, 160));
-            graphics.DrawString("Matriculationl  :I.J.G.H.S, Thrsissur 73 %, 2011", font, PdfBrushes.Black, new PointF(205, 180));
-            graphics.DrawString("Experience ", font, PdfBrushes.Blue, new PointF(205, 220));
-            graphics.DrawString("ABC Solutions 03 / 06 / 2018 - 05 / 09 / 2019", font, PdfBrushes.Black, new PointF(205, 240));
-
-            graphics.DrawString("Achievements", font, PdfBrushes.Blue, new PointF(205, 280));
-            graphics.DrawString("Developed a new employee orientation program that ", font, PdfBrushes.Black, new PointF(205, 300));
-            graphics.DrawString("100% of the company locations adopted", font, PdfBrushes.Black, new PointF(205, 320));
-
-            graphics.DrawString("Projects", font, PdfBrushes.Blue, new PointF(205, 360));
-            graphics.DrawString("Pocket Certificate , 3 / 03 / 2019 - 05 / 06 / 2019", font, PdfBrushes.Black, new PointF(205, 380));
-            graphics.DrawString("Python Django web based application to uploading and download certificate with a key", font, PdfBrushes.Black, new PointF(205, 400));
-            graphics.DrawString("download certificate with a key", font, PdfBrushes.Black, new PointF(205, 420));
+			graphics.DrawString("Name     : " + details.Name, font, PdfBrushes.Black, new PointF(240, 50));
+			
 
 
-            graphics.DrawString("Skills", font, PdfBrushes.Blue, new PointF(40, 120));
-            graphics.DrawString("Technical skills: html, css, c, c++", font, PdfBrushes.Black, new PointF(0, 140));
 
-            graphics.DrawString("Additional skills : Leadership", font, PdfBrushes.Black, new PointF(0, 160));
+
+			graphics.DrawString("Education", font, PdfBrushes.DarkBlue, new PointF(240, 100));
+            graphics.DrawString("-----------------------------", font, PdfBrushes.Black, new PointF(240, 110));
+
+            graphics.DrawString("Masters Degree      : " + details.Masters_Degree, font, PdfBrushes.Black, new PointF(240, 130));
+            graphics.DrawString("Bachelors Degree   : " + details.Bachelors_Degree, font, PdfBrushes.Black, new PointF(240, 150));
+            graphics.DrawString("Higher secondary   :" + details.Higher_secondary, font, PdfBrushes.Black, new PointF(240, 170));
+            graphics.DrawString("Matriculationl        :" + details.Matriulationlty, font, PdfBrushes.Black, new PointF(240, 190));
+
+
+
+           
+            graphics.DrawString("Experience ", font, PdfBrushes.DarkBlue, new PointF(240, 220));
+            graphics.DrawString("----------------------------", font, PdfBrushes.DarkBlue, new PointF(240, 230));
+            graphics.DrawString("Company, Location — Job Title1    :" + details.Experience, font, PdfBrushes.Black, new PointF(240, 250));
+          
+
+
+
+
+            graphics.DrawString("Achievements", font, PdfBrushes.DarkBlue, new PointF(240, 300));
+            graphics.DrawString("-------------------------", font, PdfBrushes.DarkBlue, new PointF(240, 320));
+            graphics.DrawString("Type Here   :" + details.Achievements, font, PdfBrushes.Black, new PointF(240, 330));
+
+
+
+
+
+            graphics.DrawString("Projects", font, PdfBrushes.DarkBlue, new PointF(240, 400));
+            graphics.DrawString("--------------------------------", font, PdfBrushes.DarkBlue, new PointF(240, 410));
+            graphics.DrawString("Project Name — Detail  :" + details.Projects, font, PdfBrushes.Black, new PointF(240, 430));
+            //graphics.DrawString(details.Projects, font, PdfBrushes.Black, new PointF(240, 400));
+            // graphics.DrawString("download certificate with a key", font, PdfBrushes.Black, new PointF(240, 420));
+
+
+            graphics.DrawString("Skills", font, PdfBrushes.DarkBlue, new PointF(0, 500));
+            graphics.DrawString("---------------------------", font, PdfBrushes.DarkBlue, new PointF(0, 510));
+            graphics.DrawString("Technical skills:" + details.Technical_skills, font, PdfBrushes.Black, new PointF(0, 530));
+
+            graphics.DrawString("Additional skills :" + details.Additional_skills, font, PdfBrushes.Black, new PointF(0, 560));
+
+
+
+
 
             //Saving the PDF to the MemoryStream
             MemoryStream stream = new MemoryStream();
 
+            // document.Save(stream);
+
+            //If the position is not set to '0' then the PDF will be empty.
+            // stream.Position = 0;
+
+            //Download the PDF document in the browser.
+            // FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
+            //fileStreamResult.FileDownloadName = "Anuresume.pdf";
+            //return fileStreamResult;
+
+
+
+
+
+
+
+
+            //IMAGE PDF CODE HERE
+
+
+
+
+            //Add a page to the document.
+            _ = document.Pages.Add();
+            //Create PDF graphics for the page
+            _ = page.Graphics;
+            //Load the image as stream.
+
+
+
+            FileStream imageStream = new FileStream("C:\\Users\\hp\\source\\repos\\CreatePdfFile\\CreatePdfFile\\images\\p.jpg", FileMode.Open, FileAccess.Read);
+            PdfBitmap image = new PdfBitmap(imageStream);
+            //Draw the image
+            graphics.DrawImage(image, 20, 10);
+            //Save the PDF document to stream
+            stream.Position = 0;
             document.Save(stream);
 
             //If the position is not set to '0' then the PDF will be empty.
             stream.Position = 0;
-
-            //Download the PDF document in the browser.
-            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
-            //fileStreamResult.FileDownloadName = "Anuresume.pdf";
-            //return fileStreamResult;
-            
-            
-            
-
-
-
-            
-            
-            //IMAGE PDF CODE HERE
-            
-            
-            
-            PdfDocument doc = new PdfDocument();
-			//Add a page to the document.
-			_ = doc.Pages.Add();
-			//Create PDF graphics for the page
-			_ = page.Graphics;
-			//Load the image as stream.
-			FileStream imageStream = new FileStream("C:\\Users\\hp\\AppData\\Local\\Temp\\CreatePdfFile_CoreWeb-983162689\\CreatePdfFile\\CreatePdfFile\\images\\Photo.jpg", FileMode.Open, FileAccess.Read);
-            PdfBitmap image = new PdfBitmap(imageStream);
-            //Draw the image
-            graphics.DrawImage(image, 0, 0);
-			//Save the PDF document to stream
-			_ = new MemoryStream();
-			doc.Save(stream);
-            //If the position is not set to '0' then the PDF will be empty.
-            stream.Position = 0;
             //Close the document.
-            doc.Close(true);
+            document.Close(true);
+
+
+
+
+
+
+
+
+
+
+
             //Defining the ContentType for pdf file.
             string contentType = "application/pdf";
             //Define the file name.
-            string fileName = "Resume1.pdf";
+            string fileName = "ResumeTest.pdf";
             //Creates a FileContentResult object by using the file contents, content type, and file name.
             return File(stream, contentType, fileName);
 
@@ -241,7 +264,7 @@ FileStream imageStream = new FileStream("C:\\Users\\hp\\AppData\\Local\\Temp\\Cr
             //Defining the ContentType for pdf file.
             string contentType = "application/pdf";
             //Define the file name.
-            string fileName = "Resume1.pdf";
+            string fileName = "Resume12.pdf";
             //Creates a FileContentResult object by using the file contents, content type, and file name.
             return File(stream, contentType, fileName);
         }
--- CreatePdfFile/Models/PersonalDetails.cs
+++ CreatePdfFile/Models/PersonalDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Syncfusion.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace CreatePdfFile.Models
 	{
 		public string Name{ get; set; }
 
-		public DateTime Date_birth{ get; set; }
+		public string Date_birth{ get; set; }
 		public string Email { get; set; }
 
 		public string Phone { get; set; }
@@ -18,5 +19,20 @@ namespace CreatePdfFile.Models
 		public string Country { get; set; }
 		public string State { get; set; }
 		public string City { get; set; }
+
+		public string Masters_Degree   { get; set; }
+		public string Bachelors_Degree { get; set; }
+
+		public string Higher_secondary { get; set; }
+		public string Matriulationlty { get; set; }
+
+		public string Experience { get; set; }
+		public string Achievements { get; set; }
+
+		public string Projects{ get; set; }
+		public string Technical_skills { get; set; }
+
+		public string Additional_skills{ get; set; }
+		
 	}
 }
